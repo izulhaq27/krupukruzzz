dcf476d6c457c8b8d69289a4a549a539b3a0ced5
izulhaq27 <muhammadfahrizulhahi@gmail.com>
2025-12-26 12:19:56 +0700

    update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,
+            CategorySeeder::class,
             ProductSeeder::class,
         ]);
     }
